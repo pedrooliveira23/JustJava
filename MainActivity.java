0af1b4fa4b444604460f9fbd9702876b4faf4ac4
pedrooliveira23 <pedrooliveira2309@gmail.com>
2016-01-07 17:47:16 -0200

    Populando o repositório

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.pedro.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
+    int quantidade = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        display(0);
+        displayPrice(0);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+
+    public void increment(View view) {
+        quantidade++;
+        display(quantidade);
+    }
+
+    public void decrement(View view) {
+        quantidade--;
+        display(quantidade);
+    }
+
+    public void submitOrder(View view) {
+        displayPrice(quantidade);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        String priceMessage = "Total: " + priceTextView.getText() + "\nObrigado!";
+        displayMessage(priceMessage);
+    }
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
